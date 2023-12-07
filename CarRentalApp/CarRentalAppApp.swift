2450869d899375557073e73ee93d4c3192f00927
omarthamri <omarthamri4@gmail.com>
2023-12-07 18:07:13 +0100

    Initial Commit

---
+++ CarRentalApp/CarRentalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarRentalAppApp.swift
+//  CarRentalApp
+//
+//  Created by omar thamri on 7/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarRentalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
